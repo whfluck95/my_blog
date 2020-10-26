ed6668ce38c7e78e18012d357c48d7342a494967
whfluck95 <whf956199@163.com>
2020-10-26 19:30:03 +0800

    配置模板信息

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('欢迎访问我的博客')
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客',
+        'welcome': '欢迎访问我的博客首页'
+    })
